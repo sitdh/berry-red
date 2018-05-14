455d503ba31f9b135a9d2da9558f68416e144794
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-05-15 01:44:49 +0700

    Fetch message from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+  dataPresent: any; 
+
+  dataSource = [];
+
+  sunday = new Array();
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.fetchMessageFromServer();
+  }
+
+  fetchMessageFromServer() {
+    this.http.get('http://uinames.com/api/?ext&amount=25')
+      .subscribe(data => {
+        if (null === data) return;
+
+        data.forEach(d => {
+          this.i += 1;
+          let weekd = this.weekDayName(
+            this.dateConvert(d.birthday.raw)
+          );
+          this.attachObjectToWeekday(weekd, d);
+        });
+      });
+  }
+
+  weekDayName(day: int) {
+    return this.weekDays[day];
+  }
+
+  dateConvert(rawInt: int) {
+    return (new Date(rawInt)).getDay();
+  }
+
+  attachObjectToWeekday(weekd, people) {
+    if (false === this.dataSource.hasOwnProperty(weekd)) { 
+      this.dataSource[weekd] = new Array();
+    }
+
+    this.dataSource[weekd].push(people);
+  }
 }
+
